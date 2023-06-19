bb369b25d98beb3a5e32b94f18bd2e8f3a80f01d
Jacob Baclawski <jacobb1577@gmail.com>
2023-06-19 14:29:20 -0400

    git update

--- bankmanagement.sql
+++ bankmanagement.sql
@@ -1,11 +1,7 @@
-CREATE TABLE bank(formno varchar(100), pin varchar(10), date varchar(100), type varchar(20), amount varchar(20), runningBalance int);
-
 SELECT * FROM signup;
 
 SELECT * FROM login;
 
-show tables;
-
-DROP TABLE bank;
-
 SELECT * FROM bank;
+
+SHOW tables;
